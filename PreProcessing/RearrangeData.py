4961822014a7c8f02a96302a49fa1d5678d88f80
arkanivasarkar <arkaniva101099@gmail.com>
2025-06-01 17:25:17 +0200

    added scripts to git

--- PreProcessing/RearrangeData.py
+++ PreProcessing/RearrangeData.py
@@ -1,3 +1,7 @@
+'''
+Code to read all folders exported from Oncentra and save it in different folders based on SeriesUID
+'''
+
 import os
 import glob
 import shutil
@@ -5,33 +9,35 @@ from datetime import datetime
 import pydicom
 
 
-path = r"W:\strahlenklinik\science\Physik\Arkaniva\Prostataexport Arkaniva Sarkar\Raw Data"
+inputPath = r"W:\strahlenklinik\science\Physik\Arkaniva\Prostataexport Arkaniva Sarkar\Raw Data"
 outputFolder = r"W:\strahlenklinik\science\Physik\Arkaniva\Prostataexport Arkaniva Sarkar\Filtered Data"
 
-for folderGroup in os.listdir(path):
-    for folder in os.listdir(f'{path}\\{folderGroup}'):
+for folderGroup in os.listdir(inputPath): #For all Oncentra folders
+    for folder in os.listdir(f'{inputPath}\\{folderGroup}'):   #For all patient folders
 
-        # fetch all ultrasound
-        ultrasound_files = glob.glob(f'{path}\\{folderGroup}\\{folder}\\US*')
+        # Fetch all ultrasound dicom images (files start with prefix 'US')
+        ultrasound_files = glob.glob(f'{inputPath}\\{folderGroup}\\{folder}\\US*')
+
+        # If no ultrasound dicom files detected, skip loop (first filter)
         if len(ultrasound_files)==0:
             continue
 
-        #TODO:
-        # create folder with serie
-        # compare needles and structure folder date
-        # save series
+        
+        # Read all ultrasound files in a folder
         for files in ultrasound_files:
             ds = pydicom.dcmread(files, stop_before_pixels=True)
             series_uid = ds.SeriesInstanceUID          
 
-            # make patient folder
+            # Create series folder if not already present
             if not os.path.exists(f'{outputFolder}\\{series_uid}'):
-                os.makedirs(f'{outputFolder}\\{series_uid}')
+                os.makedirs(f'{outputFolder}\\{series_uid}') # Create folder with SeriesUID
 
+                # Find needle coordinates, RTSTRUCT and dose dicoms 
+                # and move to series folder by comparing series dates and folder date
                 series_date = datetime.strptime(ds.StudyDate, '%Y%m%d').date()
 
-                for item in os.listdir(f'{path}\\{folderGroup}\\{folder}'):
-                    if not os.path.isdir(f'{path}\\{folderGroup}\\{folder}\\{item}'):
+                for item in os.listdir(f'{inputPath}\\{folderGroup}\\{folder}'):
+                    if not os.path.isdir(f'{inputPath}\\{folderGroup}\\{folder}\\{item}'):
                         continue
 
                     folderDate = datetime.strptime(item.split()[0], '%Y-%m-%d').date()
@@ -39,17 +45,17 @@ for folderGroup in os.listdir(path):
                     if series_date != folderDate:
                         continue
                     
-                    datafiles = glob.glob(f'{path}\\{folderGroup}\\{folder}\\{item}\\*')
+                    datafiles = glob.glob(f'{inputPath}\\{folderGroup}\\{folder}\\{item}\\*')
 
+                    # Move the files to series folder
                     for i in datafiles:
                         shutil.copy2(i, f'{outputFolder}\\{series_uid}\\{i.split('\\')[-1]}')
 
+
+            # Move the ultrasound DICOM to SeriesUID folder
             shutil.copy2(files, f'{outputFolder}\\{series_uid}\\{files.split('\\')[-1]}')
 
        
-            # date2_str = '2025-05-07 hPr_2025-05-07'
-            # date2 = datetime.strptime(date2_str.split()[0], '%Y-%m-%d').date()
-
            
 
 
